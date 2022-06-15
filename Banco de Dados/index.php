9355d10b892e4f7b670828a8d3190fcc977349f9
Pedro Leonardo <pedroleocnb85@gmail.com>
2022-06-15 06:53:30 -0300

    Create index.php

---
+++ Banco de Dados/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> PDO-CRUD </title>
+</head>
+<body>
+<h1> Formulario de cadastro </h1>
+<form action="inserir.php" method="POST">
+	<div>
+		<label> Nome </label>
+		<input type="text" name="nome">
+	</div>
+	<div>
+		<label>E-mail</label>
+		<input type="email" name="email">
+	</div>
+	<div>
+		<label>Senha</label>
+		<input type="password" name="senha">
+	</div>
+	<div>
+		<input type="submit" name="ENVIAR">
+	</div>
+</form>
+</body>
+</html>
